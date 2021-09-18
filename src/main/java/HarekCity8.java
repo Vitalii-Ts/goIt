509b2aee41ae2a3b4154ea1dee8dc43a8cfdd179
Vitalii-Ts <vitalii.tskhovrebashvili@gmail.com>
2021-09-18 17:12:04 +0300

    first commit

---
+++ src/main/java/HarekCity8.java
@@ -0,0 +1,23 @@
+public class HarekCity8 {
+    public String[] createEmptyNameArray() {
+        String[] result = new String[0];
+        return result;
+    }
+
+    public int[] createAgeArray(int age1, int age2, int age3, int age4) {
+        return new int[] {age1, age2, age3, age4};
+    }
+
+    public void fixNames(String[] names, String[] toReplace) {
+        names[1] = toReplace[0];
+        names[3] = toReplace[1];
+    }
+    public String firstAndLastTogether(String [] names) {
+        return (names[0]+" "+"AND"+" "+names[9]+" "+"TOGETHER");
+    }
+
+    public static void main(String[] args) {
+        String[] names = new String[]{"io", "pu", "re", "max", "kok", "mis", "hit", "kio", "pax", "rew"};
+        System.out.println(new HarekCity8().firstAndLastTogether(names));
+    }
+}
