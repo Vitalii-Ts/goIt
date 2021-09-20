0efd0133faa9d7bf0e97f0bc9cd127a584081770
Vitalii-Ts <vitalii.tskhovrebashvili@gmail.com>
2021-09-20 08:17:56 +0300

    first commit

---
+++ src/main/java/HarekCity11.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class HarekCity11 {
+    public String[] createEmptyNameArray() {
+        String[] result = new String[0];
+        return result;
+    }
+
+    public int[] createAgeArray(int age1, int age2, int age3, int age4) {
+        return new int[] {age1, age2, age3, age4};
+    }
+
+    public void fixNames(String[] names, String[] toReplace) {
+        names[1] = toReplace[0];
+        names[3] = toReplace[1];
+    }
+
+    public String firstAndLastTogether(String[] names) {
+        return names[0] + " AND " + names[9] + " TOGETHER";
+    }
+
+    public void changeElectResult(String[] results) {
+        String temp = results[0];
+        results[0] = results[results.length - 1];
+        results[results.length - 1] = temp;
+    }
+
+    public String[] changeElectResultAgain(String[] results) {
+        return new String[] {
+                results[2],
+                results[3],
+                results[4]
+        };
+    }
+    public char[][] createKeyboard() {
+        char [][] chart = {{'1','2','3'}, {'4','5','6'}, {'7','8','9'}, {'*','0','#'}};
+        return chart;
+    }
+
+    public static void main(String[] args) {
+        char[][] keyboard = new HarekCity11().createKeyboard();
+        for (int i = 0; i < keyboard.length; i++) {
+            System.out.println(Arrays.toString(keyboard[i]));
+        }
+    }
+}
